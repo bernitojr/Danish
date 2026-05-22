50a73ca0fe37200a2a2335f12757ac5364a9b42d
VirgileBernard <bernardvirgile@gmail.com>
2026-05-22 18:34:58 +0200

    refactor: remove unused imports and commented code in LandingPage component

--- src/features/landing/components/LandingPage.tsx
+++ src/features/landing/components/LandingPage.tsx
@@ -1,11 +1,8 @@
-import { useNavigate } from 'react-router-dom'
-import { useGameStore } from '../../game/store/gameStore'
 import { Hero } from './Hero'
 import { QuickAccessCards } from './QuickAccessCard'
 
 export function LandingPage() {
   // const store = useGameStore()
-  const navigate = useNavigate()
 
   return (
     <div>
